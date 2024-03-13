b1d503cbd15e56dd870692e2e6062da6bcaefe82
vidhi3839 <vidhipatel742003@gmail.com>
2024-03-13 10:34:50 +0530

    Guide Dashboard

---
+++ FrontendPG/src/components/Guide/OngoingGuideDissertation/OngoingGuideDissertation.jsx
@@ -0,0 +1,77 @@
+import './OngoingGuideDissertation.css';
+const OngoingGuideDissertation = ()=>{
+    return (
+        <>
+        <div id="gongoing_dissertation">
+        <div className="guidereq_head">
+            <div className="guidereq_img">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6h97cyJOLha0BuEZSM6RgwHaE8&pid=Api&rs=1&c=1&qlt=105&w=145&h=97" alt="" />
+            </div>
+                <div>
+                <h2>Abc </h2>
+                <p>Phd in Phycology</p>
+                </div>
+            </div>
+<h2>Ongoing Dissertations</h2>
+<div className="guide_ongoingD">
+    <div className="guide_ongoingDis">
+        <div className="stu_img">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6h97cyJOLha0BuEZSM6RgwHaE8&pid=Api&rs=1&c=1&qlt=95&w=145&h=97" alt="" />
+        </div>
+        <div className="ongoing_content">
+            <p>Topic: Lorem ipsum dolor sit amet</p>
+            <p>Name: ABC XYZ</p>
+            <p>Email: abc@gmail.com</p>
+            <button>View More</button>
+        </div>
+    </div>
+    <div className="guide_ongoingDis">
+        <div className="stu_img">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6h97cyJOLha0BuEZSM6RgwHaE8&pid=Api&rs=1&c=1&qlt=95&w=145&h=97" alt="" />
+        </div>
+        <div className="ongoing_content">
+            <p>Topic: Lorem ipsum dolor sit amet</p>
+            <p>Name: ABC XYZ</p>
+            <p>Email: abc@gmail.com</p>
+            <button>View More</button>
+        </div>
+    </div>
+    <div className="guide_ongoingDis">
+        <div className="stu_img">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6h97cyJOLha0BuEZSM6RgwHaE8&pid=Api&rs=1&c=1&qlt=95&w=145&h=97" alt="" />
+        </div>
+        <div className="ongoing_content">
+            <p>Topic: Lorem ipsum dolor sit amet</p>
+            <p>Name: ABC XYZ</p>
+            <p>Email: abc@gmail.com</p>
+            <button>View More</button>
+        </div>
+    </div>
+    <div className="guide_ongoingDis">
+        <div className="stu_img">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6h97cyJOLha0BuEZSM6RgwHaE8&pid=Api&rs=1&c=1&qlt=95&w=145&h=97" alt="" />
+        </div>
+        <div className="ongoing_content">
+            <p>Topic: Lorem ipsum dolor sit amet</p>
+            <p>Name: ABC XYZ</p>
+            <p>Email: abc@gmail.com</p>
+            <button>View More</button>
+        </div>
+    </div>
+    <div className="guide_ongoingDis">
+        <div className="stu_img">
+            <img src="https://tse1.mm.bing.net/th?id=OIP.6h97cyJOLha0BuEZSM6RgwHaE8&pid=Api&rs=1&c=1&qlt=95&w=145&h=97" alt="" />
+        </div>
+        <div className="ongoing_content">
+            <p>Topic: Lorem ipsum dolor sit amet</p>
+            <p>Name: ABC XYZ</p>
+            <p>Email: abc@gmail.com</p>
+            <button>View More</button>
+        </div>
+    </div>
+</div>
+</div>
+        </>
+    )
+}
+export default OngoingGuideDissertation;
